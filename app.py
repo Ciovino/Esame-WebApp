cfeab64c8c7315914eb3dff16841f9fb3dc1ac92
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-01-11 18:15:26 +0100

    aggiunta pagina login

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ def homepage():
 def registrati():
     return render_template('registrati.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    return redirect(url_for('homepage'))
+
 @app.route('/add_new_user', methods=['POST'])
 def add_new_user():
     return redirect(url_for('homepage'))
