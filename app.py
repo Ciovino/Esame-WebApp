6a0928050458ae9f0b65bf5d449799492b2b93b1
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-01-12 18:47:13 +0100

    controllo server side del form di registrazione

--- app.py
+++ app.py
@@ -19,4 +19,48 @@ def login():
 
 @app.route('/add_new_user', methods=['POST'])
 def add_new_user():
-    return redirect(url_for('homepage'))
+    new_user = request.form.to_dict()
+
+    # Nome utente univoco
+    if not utente_univoco(new_user.get('username')):
+        # Messaggio di errore
+        return redirect(url_for('registrati'))
+    
+    # Email univoca
+    if not email_univoca(new_user.get('email')):
+        # Messaggio di errore
+        return redirect(url_for('registrati'))
+    
+    # TODO: Cripta password
+
+    # Ascoltatore o Creatore
+    if new_user.get('tipo_utente') == 'Ascoltare':
+        new_user['tipo_utente'] = 0
+    elif new_user.get('tipo_utente') == 'Creare':
+        new_user['tipo_utente'] = 1
+    else:
+        # Errore
+        return redirect(url_for('registrati'))
+    
+    # Immagine profilo
+    new_user['immagine_profilo'] = 'Prova'
+
+    if not aggiungi_utente_database(new_user):
+        # Errore
+        return redirect(url_for('registrati'))
+
+    # Successo
+    return redirect(url_for('login'))
+
+# Controlla che l'username sia univoco all'interno del database
+def utente_univoco(username):
+    return True
+
+# Controlla che la email sia univoca all'interno del database
+def email_univoca(email):
+    return True
+
+# Aggiunge al databse
+def aggiungi_utente_database(utente):
+    app.logger.info(utente)
+    return True
